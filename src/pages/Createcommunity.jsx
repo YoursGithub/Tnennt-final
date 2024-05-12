091cef32e1ae0ef7eb2b8101f8f4fe4634f6f028
YoursGithub <164144751+YoursGithub@users.noreply.github.com>
2024-05-12 14:54:48 -0400

    Update Createcommunity.jsx

--- src/pages/Createcommunity.jsx
+++ src/pages/Createcommunity.jsx
@@ -43,16 +43,16 @@ function CommunityPost() {
 
     return (
         <section className="lg:hidden">
-            <div className="flex justify-between mt-6">
-                <h2 className="font-extrabold text-[5vw] ml-4 mt-4">COMMUNITY POST<span className="text-red-500"> &bull;</span></h2>
+            <div className="flex justify-between items-center mt-6">
+                <h2 className="font-extrabold text-[6vw] ml-4">COMMUNITY POST<span className="text-red-500"> &bull;</span></h2>
                 <div className="w-[12.7vw] h-[12.7vw] bg-[#F5F5F5] rounded-full flex-shrink-0 flex justify-center items-center mr-4 ">
                     <img className="w-[2.5vw] mr-[4.1px]" src={image1} alt="" />
                 </div>
             </div>
             <div>
-                <div className='flex items-center gap-3'>
-                    <h2 className="font-extrabold text-[5vw] mt-16 ml-3">Add Images</h2>
-                    <h2 className='mt-[16.4%] text-[3.5vw]'>(optional)</h2>
+                <div className='flex items-center gap-3 mt-[7%] ml-4'>
+                    <h2 className="font-extrabold text-[5vw] ">Add Images</h2>
+                    <h2 className=' text-[3.5vw] mt-1'>(optional)</h2>
                     </div>
                     <div className="flex ml-4 items-center mt-6 gap-3">
                         <div className="w-[25vw] h-[25vw] border-2 border-dashed flex justify-center items-center rounded-2xl border-[#848484]">
@@ -61,9 +61,9 @@ function CommunityPost() {
                             <input id="imageUpload1" type="file" accept="image/*" className="hidden" onChange={handleImageUpload1} />
                         </label>
                         </div>
-                        <p className="text-[3.2vw]">(Add more than one image of the product)</p>
+                        {/* <p className="text-[3.2vw]">(Add more than one image of the product)</p> */}
                     </div>
-                    <h2 className="font-extrabold text-[5vw] mt-7 ml-3">Caption</h2>
+                    <h2 className="font-extrabold text-[5vw] mt-7 ml-4">Caption</h2>
             <div className="flex justify-center">
                 <div className="w-[93vw] h-[34vw] mt-6 border-2 border-dashed  rounded-2xl border-[#848484] flex flex-col ">
                     <h2 className="font-bold ml-3 mt-1 text-[3.7vw] text-[#545454]">Description</h2>
@@ -77,7 +77,7 @@ function CommunityPost() {
             </div>
             <p className="ml-[82%] mt-1 text-[3.6vw]">({description.length}/{maxLength})</p>
             <div className='flex items-center gap-3'>
-            <h2 className='font-extrabold text-[5vw]  ml-3'>Product Link</h2>
+            <h2 className='font-extrabold text-[5vw]  ml-4'>Product Link</h2>
             <h2 className='text-[3.5vw] mt-[0.7%]'>(optional)</h2>
             </div>
             <div className='px-3 py-5 flex items-center gap-2 w-[93vw] ml-3 mt-5 rounded-2xl h-[10vw] border-[1px] border-[#B7B7B7]'>
