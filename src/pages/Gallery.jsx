b802120f22472941140d2e8542bdb861dad2bc58
YoursGithub <164144751+YoursGithub@users.noreply.github.com>
2024-05-12 14:55:25 -0400

    Update Gallery.jsx

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -24,23 +24,23 @@ function Gallery() {
                 <div className="flex p-[20px] justify-between">
                     <div className="flex justify-center items-center gap-2">
                         <h2 className="text-[5.7vw] font-bold">GALLERY</h2>
-                        <div className="bg-[#00FF44] h-[1vh] w-[1vh] rounded-full"></div>
+                        <div className="bg-[#00FF44] h-[0.8vh] w-[0.8vh] rounded-full"></div>
                     </div>
-                    <button className="bg-black p-4 py-3 rounded-lg text-white text-[3.7vw]">Contact Us</button>
+                    <button className="bg-black p-4 py-2 rounded-lg text-white text-[3.7vw]">Contact Us</button>
                 </div>
                 <h3 className="font-bold text-[5vw] mt-5 ml-4">Store</h3>
                 <div className="flex justify-center">
-                    <div className="w-[93vw] rounded-xl mt-4 bg-[#2D332F] py-5 px-3 items-center relative">
+                    <div className="w-[93vw] rounded-xl mt-3 bg-[#2D332F] py-5 px-3 items-center relative">
                         <Link to='/storeprofile'>
                         <div className="flex gap-3 items-center">
                             <div className="rounded-lg w-[9vh] h-[9vh] bg-green-200 bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url(${xyzstore})` }}></div>
                             <div className="flex flex-col">
                                 <div className="flex items-center gap-2">
-                                    <h2 className="text-white font-bold text-[7vw]">XYZ Store</h2>
+                                    <h2 className="text-white font-bold text-[6vw]">XYZ Store</h2>
                                     <div className="rounded-full w-[0.8vh] h-[0.8vh] bg-green-500 mt-2"></div>
                                 </div>
                                 <div className="flex gap-2 items-center">
-                                    <img className="w-[1.3vh]" src={vector5} alt="" />
+                                    <img className="w-[1.3vh] mt-1" src={vector5} alt="" />
                                     <p className="text-white opacity-50 text-[3vw]">xyzstore.tnennt.store</p>
                                 </div>
                             </div>
